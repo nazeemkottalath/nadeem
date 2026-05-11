e06b22561bc0325b08f690aae709583a1223c9fb
nazeemkottalath <109637820+nazeemkottalath@users.noreply.github.com>
2026-05-11 11:13:14 -0700

    Update supabase.js

--- advnadeem/src/lib/supabase.js
+++ advnadeem/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https:/ryudgscczrylbyefgexk.supabase.co'
+const supabaseUrl = 'https://ryudgscczrylbyefgexk.supabase.co'
 const supabaseAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5dWRnc2NjenJ5bGJ5ZWZnZXhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTAwODUsImV4cCI6MjA5NDA4NjA4NX0.E2D-Wy8RtdF8rEDpb9V-xkpJeDjT1jYV8OWti5wQtNI'
 
 export const supabase = createClient(supabaseUrl, supabaseAnon)
